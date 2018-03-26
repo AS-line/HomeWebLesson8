d06f47b39624cdec1845caeb3cd1f0811a274094
As-line <taibov.0321@mail.ru>
2018-03-27 00:50:52 +0300

    fixes

--- HomeWebLesson8/settings.py
+++ HomeWebLesson8/settings.py
@@ -117,9 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    '/var/www/static/',
-]
+    os.path.join(BASE_DIR, "static"), '/var/www/static/', ]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
