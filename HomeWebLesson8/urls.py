ca97b9164d58f00d0ba89442069eaed642eee64d
AS-line <taibov.0321@mail.ru>
2018-03-26 13:09:54 +0300

    fixed detail view

--- HomeWebLesson8/urls.py
+++ HomeWebLesson8/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
-from main.views import ArticleDetailView, ArticleListView
+from main.views import ArticleDetailView, ArticleListView, HomeView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView.as_view(), name="home"),
     path("news/", ArticleListView.as_view(), name='list'),
     path("news/<slug:slug>", ArticleDetailView.as_view(), name='detail'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from .models import Article, Tag
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["pages"] = Tag.objects.all()
+        return context
 
 
 class ArticleListView(ListView):
@@ -7,8 +16,9 @@ class ArticleListView(ListView):
     model = Article
 
     def get_context_data(self, **kwargs):
+        news_list = Article.objects.all()
         context = super().get_context_data(**kwargs)
-        context["all_news"] = Article.objects.all()
+        context["all_news"] = news_list
         context["pages"] = Tag.objects.all()
         return context
 
@@ -16,9 +26,9 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     template_name = "detail.html"
     model = Article
+    context_object_name = "new"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["new"] = Article.objects.get()
         context["pages"] = Tag.objects.all()
         return context
